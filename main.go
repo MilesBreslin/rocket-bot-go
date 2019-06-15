50a4ba7bc61c3496c51eca3a1eca115532d233ce
Miles Breslin <milesbreslin@gmail.com>
2019-06-15 11:50:05 -0700

    iSayGo init

--- main.go
+++ main.go
@@ -12,19 +12,43 @@ import (
     "./rocket"
 
     "fmt"
-    "time"
+    "io/ioutil"
+    "os"
+    "strings"
     "gopkg.in/yaml.v2"
 )
 
+type entry struct {
+    Statement           string
+    Reply               string
+    UserName            string
+    RoomId              string
+    MsgId               string
+}
+
+var entries []entry
+var filename = "iSay.cfg"
+
+func init() {
+    entries = make([]entry, 0)
+    _, err := os.Stat(filename)
+    if err != nil {
+        return
+    }
+
+    source, err := ioutil.ReadFile(filename)
+    if err != nil {
+        return
+    }
+
+    err = yaml.Unmarshal(source, &entries)
+    if err != nil {
+        return
+    }
+}
+
 func main() {
-    // New Connection returning a rocketConnection object
-    // rb.cfg is backwards compatible with Kimani's rocket-bot-python
-    // Also see NewConnectionPassword and NewConnectionAuthToken
     rock, err := rocket.NewConnectionConfig("rb.cfg")
-
-    rock.UserTemporaryStatus(rocket.STATUS_AWAY)
-
-    // If there was an error connecting, panic
     if err != nil {
         panic(err)
     }
@@ -32,36 +56,94 @@ func main() {
     for {
         // Wait for a new message to come in
         msg, err := rock.GetNewMessage()
-
-        // If error, quit because that means the connection probably quit
         if err != nil {
             break
         }
 
-        // Print the message structure in a user-legible format
-        // yml is []byte type, _ means send the returned error to void
-        yml, _ := yaml.Marshal(msg)
-        fmt.Println(string(yml))
-
-        // If begins with '@Username ' or is in private chat
-        if msg.IsAddressedToMe || msg.RoomName == "" {
-            // Reply to the message with a formatted string
-            reply, err := msg.Reply(fmt.Sprintf("@%s %s %d", msg.UserName, msg.GetNotAddressedText(), 0))
-
-            // If no error replying, take the reply and edit it to count to 10 asynchronously
-            if err == nil {
-                go func() {
-                    msg.SetIsTyping(true)
-                    for i := 1; i<=10; i++ {
-                        time.Sleep(time.Second)
-                        reply.EditText(fmt.Sprintf("@%s %s %d", msg.UserName, msg.GetNotAddressedText(),i))
-                    }
-                    msg.SetIsTyping(false)
-                }()
+        var statement string
+        var reply string
+        _, err = fmt.Sscanf(msg.GetNotAddressedText(), "when i say %s you say %s", &statement, &reply)
+        if err == nil {
+            if statement[len(statement)-1:len(statement)] == "," {
+                statement=statement[:len(statement)-1]
             }
-
-            // React to the initial message
-            msg.React(":grinning:")
+            if reply[len(reply)-1:len(reply)] == "." {
+                reply=reply[:len(reply)-1]
+            }
+            entries = append(entries, entry{
+                Statement: statement,
+                Reply: reply,
+                UserName: msg.UserName,
+                RoomId: msg.RoomId,
+                MsgId: msg.Id,
+            })
+            writeDatabase()
+        } else {
+            fmt.Println(err)
+            if msg.IsAddressedToMe {
+                success := false
+                parsableText := strings.ToLower(msg.GetNotAddressedText())
+                if postfix := getTextAfter(parsableText, "delete "); postfix != "" {
+                    if postfix := getTextAfter(postfix, " i say "); postfix != "" {
+                        for i := 0; i<len(entries); i++ {
+                            if strings.ToLower(entries[i].Statement) == postfix {
+                                success = true
+                                rock.React(entries[i].MsgId, ":skull_crossbones:")
+                                msg.Reply(fmt.Sprintf("%s\n--Delted--",entries[i].toText()))
+                                entries[i] = entries[len(entries)-1]
+                                entries = entries[:len(entries)-1]
+                                break
+                                i--
+                            }
+                        }
+                    }
+                    if postfix := getTextAfter(postfix, " you say "); postfix != "" {
+                        for i := 0; i<len(entries); i++ {
+                            if strings.ToLower(entries[i].Reply) == postfix {
+                                success = true
+                                rock.React(entries[i].MsgId, ":skull_crossbones:")
+                                msg.Reply(fmt.Sprintf("%s\n--Delted--",entries[i].toText()))
+                                entries[i] = entries[len(entries)]
+                                entries = entries[:len(entries)-1]
+                                i--
+                            }
+                        }
+                    }
+                    if success {
+                        writeDatabase()
+                    } else {
+                        msg.Reply("I didn't understand you.\nDelete when {i,you} say \\_\\_\\_")
+                    }
+                } else {
+                    msg.Reply("Speak properly!\nWhen I say \\_\\_\\_, you say \\_\\_\\_.\nYou can also say \"@"+rock.UserName+" delete when {I,you} say \\_\\_\\_")
+                }
+            }
         }
     }
+}
+
+func writeDatabase() {
+    yml, err := yaml.Marshal(entries)
+    if err != nil {
+        return
+    }
+    fmt.Println(string(yml))
+
+    err = ioutil.WriteFile(filename, yml, 0644)
+    if err != nil {
+        return
+    }
+}
+
+func getTextAfter(initial string, match string) (string) {
+    if index := strings.Index(initial, match); index != -1 {
+        return initial[index+len(match):]
+    } else {
+        return ""
+    }
+}
+
+func (e *entry) toText() (string) {
+    yml, _ := yaml.Marshal(e)
+    return string(yml)
 }
